78185f51c80b444e086c6e5912aadd4692d4e3fb
Ahmet <65831285+BristolMyers@users.noreply.github.com>
2020-06-18 18:26:14 +0300

    Updated by BristolMyers

---
+++ userbot/modules/fakemail.py
@@ -0,0 +1,72 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+# Cete UserBot - BristolMyers
+
+""" İnsanlarla eğlenmek için yapılmış olan UserBot modülü. """
+
+from asyncio import sleep
+from random import choice, getrandbits, randint
+from re import sub
+import time
+from collections import deque
+import requests
+from userbot import CMD_HELP
+from userbot.events import register
+from userbot.modules.admin import get_user_from_event
+from telethon import events
+import asyncio
+import os
+import sys
+import random
+from telethon import events, functions, types
+import asyncio
+from telethon.tl.types import ChannelParticipantsAdmins
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+@register(outgoing=True, pattern="^.fakemail")
+async def fakemail(event):
+   if event.fwd_from:
+      return
+   chat = "@fakemailbot"
+   command = "/generate"
+   await event.edit("```Fakemail Oluşturuluyor lütfen bekle CeteUserBot...```")
+   async with event.client.conversation(chat) as conv:
+      try:
+         m = await event.client.send_message("@fakemailbot","/generate")
+         await asyncio.sleep(5)
+         k = await event.client.get_messages(entity="@fakemailbot", limit=1, reverse=False)
+         mail = k[0].text
+         # print(k[0].text)
+      except YouBlockedUserError:
+         await event.reply("```Lütfe @fakemailbot engellemesini kaldırın tekrar deneyin```")
+         return
+      await event.edit(mail)
+
+@register(outgoing=True, pattern="^.mailid")
+async def mailid(event):
+   if event.fwd_from:
+      return
+   chat = "@fakemailbot"
+   command = "/id"
+   await event.edit("```Fakemail listesi getiriliyor lütfen bekleyin```")
+   async with event.client.conversation(chat) as conv:
+        try:
+            m = await event.client.send_message("@fakemailbot","/id")
+            await asyncio.sleep(5)
+            k = await event.client.get_messages(entity="@fakemailbot", limit=1, reverse=False)
+            mail = k[0].text
+            # print(k[0].text)
+        except YouBlockedUserError:
+            await event.reply("```Lütfe @fakemailbot engellemesini kaldırın tekrar deneyin```")
+            return
+        await event.edit(mail)
