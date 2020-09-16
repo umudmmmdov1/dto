1dca4bd64216d151ca58ddb37fddb66af64ffeb5
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-09-16 08:00:12 +0400

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -200,8 +200,10 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
 
 # Genius modülünün çalışması için buradan değeri alın https://genius.com/developers her ikisi de aynı değerlere sahiptir
 GENIUS = os.environ.get("GENIUS", None)
-CMD_HELP = {}
+CMD_HELP = {}b
 
+SPOTIFY_DC = os.environ.get("SPOTIFY_DC", None)
+SPOTIFY_KEY = os.environ.get("SPOTIFY_KEY", None)
 
 # CloudMail.ru ve MEGA.nz ayarlama
 if not os.path.exists('bin'):
