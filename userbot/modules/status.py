d52556730ebb13bbef6ce8b31d9db487a9448287
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-10-28 23:18:56 +0400

    Update status.py

--- userbot/modules/status.py
+++ userbot/modules/status.py
@@ -1,12 +1,12 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020 
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# 
 
-from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, ASENA_VERSION
+from userbot import CMD_HELP, ASYNC_POOL, tgbot, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, DTO_VERSION
 from userbot.events import register
 from telethon import version
 from platform import python_version
@@ -38,7 +38,6 @@ async def durums(event):
 **{LANG['PLUGIN_COUNT']}:** `{len(CMD_HELP)}`
 
 **Inline Bot:** `{durum(tgbot)}`
-**Spotify:** `{durum(SPOTIFY_DC)}`
 **GDrive:** `{durum(G_DRIVE_CLIENT_ID)}`
 **LastFm:** `{durum(lastfm)}`
 **YouTube ApiKey:** `{durum(YOUTUBE_API_KEY)}`
