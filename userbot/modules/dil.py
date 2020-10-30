fb3e6ae3bbf7ca4675db7854c264d447e91bf4bd
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-10-31 02:09:48 +0400

    Update dil.py

--- userbot/modules/dil.py
+++ userbot/modules/dil.py
@@ -1,11 +1,12 @@
-# Copyright (C) 2020 Ümüd
+# Copyright (C) 2020 Yusuf Usta.
 #
 # Licensed under the  GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# DTÖUserBot - Ümüd
+# Asena UserBot - Yusuf Usta
 
+from userbot.cmdhelp import CmdHelp
 from userbot import PLUGIN_CHANNEL_ID, CMD_HELP
 from userbot.events import register
 from re import search
@@ -25,13 +26,13 @@ async def dil(event):
             reply = await event.get_reply_message()
             dosya = await reply.download_media()
 
-            if ((len(reply.file.name.split(".")) >= 2) and (not reply.file.name.split(".")[1] == "dtojson")):
-                return await event.edit("`Lütfen geçerli bir` **DTOJSON** `dosyası verin!`")
+            if ((len(reply.file.name.split(".")) >= 2) and (not reply.file.name.split(".")[1] == "asenajson")):
+                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
 
             try:
                 dosya = loads(open(dosya, "r").read())
             except JSONDecodeError:
-                return await event.edit("`Lütfen geçerli bir` **DTOJSON** `dosyası verin!`")
+                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
 
             await event.edit(f"`{dosya['LANGUAGE']}` `dili yükleniyor...`")
             pchannel = await event.client.get_entity(PLUGIN_CHANNEL_ID)
@@ -49,19 +50,19 @@ async def dil(event):
         if event.is_reply:
             reply = await event.get_reply_message()
             if ((len(reply.file.name.split(".")) >= 1) and (not reply.file.name.split(".")[1] == "asenajson")):
-                return await event.edit("`Lütfen geçerli bir` **DTOJSON** `dosyası verin!`")
+                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
 
             dosya = await reply.download_media()
 
             try:
                 dosya = loads(open(dosya, "r").read())
             except JSONDecodeError:
-                return await event.edit("`Lütfen geçerli bir` **DTOJSON** `dosyası verin!`")
+                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
 
             await event.edit(
                 f"**Dil: **`{dosya['LANGUAGE']}`\n"
                 f"**Dil Kodu: **`{dosya['LANGCODE']}`\n"
-                f"**Çevirmen: **`{dosya['AUTHOR']}`\n"
+                f"**Tərcüməçi: **`{dosya['AUTHOR']}`\n"
 
                 f"\n\n`Dil dosyasını yüklemek için` `.dil yükle` `komutunu kullanınız.`"
             )
@@ -71,17 +72,15 @@ async def dil(event):
         await event.edit(
             f"**Dil: **`{LANGUAGE_JSON['LANGUAGE']}`\n"
             f"**Dil Kodu: **`{LANGUAGE_JSON['LANGCODE']}`\n"
-            f"**Çevirmen: **`{LANGUAGE_JSON ['AUTHOR']}`\n"
+            f"**Tərcüməci: **`{LANGUAGE_JSON ['AUTHOR']}`\n"
 
-            f"\n\nDiğer diller için @DTODil kanalına bakabilirsiniz."
+            f"\n\nDiğer diller için @AsenaDil kanalına bakabilirsiniz."
         )
 
-CMD_HELP.update({
-    "dil":
-    "`.dil`\
-    \n**Kullanım:** `Yüklediğiniz dil hakkında bilgi verir.`\
-    `.dil bilgi`\
-    \n**Kullanım:** `Yanıt verdiğiniz dil dosyası hakkında bilgi verir`\
-    `.dil yükle`\
-    \n**Kullanım:** `Yanıt verdiğiniz dil dosyasını yükler`"
-})
+CmdHelp('dil').add_command(
+    'dil', None, 'Yüklediğiniz dil hakkında bilgi verir.'
+).add_command(
+    'dil bilgi', None, 'Yanıt verdiğiniz dil dosyası hakkında bilgi verir.'
+).add_command(
+    'dil yükle', None, 'Yanıt verdiğiniz dil dosyasını yükler.'
+).add()
