2d0cc303136b872787c98915eed936af6275f3d1
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-10-30 23:11:05 +0400

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -1,21 +1,12 @@
-# Copyright (C) 2020 TeamDerUntergang.
+# Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
 #
 
-""" Olayları yönetmek için UserBot modülü.
- UserBot'un ana bileşenlerinden biri. """
+# DTÖUserBot - Ümüd
+
+""" """
 
 import sys
 from asyncio import create_subprocess_shell as asyncsubshell
@@ -26,23 +17,20 @@ from traceback import format_exc
 
 from telethon import events
 
-from userbot import bot, BOTLOG_CHATID, LOGSPAMMER, SILINEN_PLUGIN
+from userbot import bot, BOTLOG_CHATID, LOGSPAMMER, PATTERNS
 
 
 def register(**args):
-    """ Yeni bir etkinlik kaydedin. """
+    """  """
     pattern = args.get('pattern', None)
     disable_edited = args.get('disable_edited', False)
-    ignore_unsafe = args.get('ignore_unsafe', False)
-    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
     groups_only = args.get('groups_only', False)
     trigger_on_fwd = args.get('trigger_on_fwd', False)
     trigger_on_inline = args.get('trigger_on_inline', False)
     disable_errors = args.get('disable_errors', False)
 
-    if pattern is not None and not pattern.startswith('(?i)'):
-        args['pattern'] = '(?i)' + pattern
-
+    if pattern:
+        args["pattern"] = pattern.replace("^.", "^["+ PATTERNS + "]")
     if "disable_edited" in args:
         del args['disable_edited']
 
@@ -61,10 +49,6 @@ def register(**args):
     if "trigger_on_inline" in args:
         del args['trigger_on_inline']
 
-    if pattern:
-        if not ignore_unsafe:
-            args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
-
     def decorator(func):
         async def wrapper(check):
             if not LOGSPAMMER:
@@ -79,7 +63,7 @@ def register(**args):
                 return
              
             if groups_only and not check.is_group:
-                await check.respond("`Zəhmət olmasa qrupda yazın.`")
+                await check.respond("`Bunun bir qrup olduğuna inanmıram.`")
                 return
 
             try:
@@ -91,35 +75,33 @@ def register(**args):
             except KeyboardInterrupt:
                 pass
             except BaseException:
-
-
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**USERBOT XƏTA**\n"
-                    link = "[DTÖUserBot Dəstək Qrupu](https://t.me/DTOSupport)"
-                    text += "İstəyirsizsə, bunu şikayət edə bilərsiz"
-                    text += f"- sadəcə bu mesajı bura yönləndirin {link}.\n"
-                    text += "Xətadan başqa heçbir şey qeyd olunmur!\n"
+                    text = "**DTÖUSERBOT XETA**\n"
+                    link = "[DTÖUserBot Dəstək Qrupu](https://t.me/DtoSupport)"
+                    text += "İstərsəniz, bunu şikayət edə bilərsiz"
+                    text += f"- sədəcə bu mesajı buraya yönləndirin {link}.\n"
+                    text += "Xəta və Tarix xaricində heç birşey qeyd edilmir\n"
 
                     ftext = "========== XEBERDARLIQ =========="
-                    ftext += "\nBu fayl sadece burada yüklendi,"
-                    ftext += "\nsadece xeta ve tarix qismink qeyd eledik,"
+                    ftext += "\nBu fayl sadece burada yüklenib,"
+                    ftext += "\nsadece xeta ve tarix yerini qeyd etdik,"
                     ftext += "\ngizliliyinize hörmet edirik,"
-                    ftext += "\nburada her hansı bir gizli veri varsa"
-                    ftext += "\nbu xeta şikayetj olmaya biler, kimse verilerinize oğurlayamaz.\n"
+                    ftext += "\nburada her hansı bir gizli melumat ola bilmez."
+                    ftext += "\nbu xeta şikayeti olmaya biler, kimse melumatlarinizi ogurlaya bilmez.\n"
                     ftext += "================================\n\n"
                     ftext += "--------USERBOT XETA GUNLUYU--------\n"
                     ftext += "\nTarix: " + date
                     ftext += "\nQrup ID: " + str(check.chat_id)
-                    ftext += "\nGönderen istifadecinin ID: " + str(check.sender_id)
+                    ftext += "\nGönderen istifadeçinin ID: " + str(check.sender_id)
                     ftext += "\n\nHadise:\n"
                     ftext += str(check.text)
-                    ftext += "\n\nMelumat:\n"
+                    ftext += "\n\nGeri izleme melumatı:\n"
                     ftext += str(format_exc())
-                    ftext += "\n\nHata metni:\n"
+                    ftext += "\n\nXeta metini:\n"
                     ftext += str(sys.exc_info()[1])
-                    ftext += "\n\n--------USERBOT XETA GUNLUYU BITISI--------"
+                    ftext += "\n\n--------USERBOT XETA GUNLUYU BİTİS--------"
 
                     command = "git log --pretty=format:\"%an: %s\" -10"
 
@@ -139,8 +121,8 @@ def register(**args):
                     file.close()
 
                     if LOGSPAMMER:
-                        await check.client.respond("`Pis xəbərim var :( , UserBotun çökdü.\
-                        \nXəta günlükləri UserBot günlük qrupunda saxlanır.`")
+                        await check.client.respond("`Təəsüf, UserBot'um çökdü.\
+                        \nXəta günlükləri DTÖUserBot günlük qrupunda saxlanılır.`")
 
                     await check.client.send_file(send_to,
                                                  "error.log",
