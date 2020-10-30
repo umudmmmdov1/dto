031275dfd66881b9b0986de90b93292332c61f8e
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-10-30 21:41:15 +0400

    Update and rename mahni.py to song.py

--- userbot/modules/song.py
+++ userbot/modules/song.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2020 
+#
+# Licensed under the GPL-3.0 License;
+# you may not use this file except in compliance with the License.
+#
+
+# DTÖUserBot - Ümüd
+
 import datetime
 import asyncio
 from telethon import events
@@ -8,39 +16,56 @@ from userbot.events import register
 import os
 import subprocess
 import glob
+from random import randint
+from userbot.cmdhelp import CmdHelp
 
-@register(outgoing=True, pattern="^.mahni(?: |$)(.*)")
+# ██████ LANGUAGE CONSTANTS ██████ #
+
+from userbot.language import get_value
+LANG = get_value("song")
+
+# ████████████████████████████████ #
+
+@register(outgoing=True, pattern="^.deez(\d*|)(?: |$)(.*)")
 async def deezl(event):
     if event.fwd_from:
         return
-    sarki = event.pattern_match.group(1)
+    sira = event.pattern_match.group(1)
+    if sira == '':
+        sira = 0
+    else:
+        sira = int(sira)
+
+    sarki = event.pattern_match.group(2)
     if len(sarki) < 1:
         if event.is_reply:
             sarki = await event.get_reply_message().text
         else:
-            await event.edit("**Mənə bir mahnı adı ver!** `İşlədilişi: .mahni musiqi adı/youtube/spotify/soundcloud`") 
+            await event.edit(LANG['GIVE_ME_SONG']) 
 
-    await event.edit("`Musiqi axtarılır...`")
+    await event.edit(LANG['SEARCHING'])
     chat = "@DeezerMusicBot"
     async with bot.conversation(chat) as conv:
         try:     
-            await conv.send_message(sarki)
+            mesaj = await conv.send_message(str(randint(31,62)))
+            sarkilar = await conv.get_response()
+            await mesaj.edit(sarki)
+            sarkilar = await conv.get_response()
         except YouBlockedUserError:
-            await event.reply(f"`Mmmh deyəsən` {chat} `bloklamısan. zəhmət olmasa blokdan aç.`")
+            await event.reply(LANG['BLOCKED_DEEZER'])
             return
-        sarkilar = await conv.wait_event(events.NewMessage(incoming=True,from_users=595898211))
         await event.client.send_read_acknowledge(conv.chat_id)
         if sarkilar.audio:
             await event.client.send_read_acknowledge(conv.chat_id)
-            await event.client.send_message(event.chat_id, f"@DTOUserBot ilə yükləndi.", file=sarkilar.message)
+            await event.client.send_message(event.chat_id, LANG['UPLOADED_WITH'], file=sarkilar.message)
             await event.delete()
         elif sarkilar.buttons[0][0].text == "No results":
-            await event.edit("`Axtardığınız musiqi tapılmadı ! Təəsüf 😓`")
+            await event.edit(LANG['NOT_FOUND'])
         else:
-            await sarkilar.click(0)
+            await sarkilar.click(sira)
             sarki = await conv.wait_event(events.NewMessage(incoming=True,from_users=595898211))
             await event.client.send_read_acknowledge(conv.chat_id)
-            await event.client.send_message(event.chat_id, f"`{sarkilar.buttons[0][0].text}` | @DTOUserBot ilə yükləndi.", file=sarki.message)
+            await event.client.send_message(event.chat_id, f"`{sarkilar.buttons[sira][0].text}` | " + LANG['UPLOADED_WITH'], file=sarki.message)
             await event.delete()
 
 @register(outgoing=True, pattern="^.song(?: |$)(.*)")
@@ -50,20 +75,20 @@ async def port_song(event):
     
     cmd = event.pattern_match.group(1)
     if len(cmd) < 1:
-        await event.edit("`İşlədilişi: .song musiqi adı/youtube url/spotify url`") 
+        await event.edit(LANG['UPLOADED_WITH']) 
 
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
         
-    await event.edit("`Musiqi axtarılır və yüklənilir zəhmət olmasa gözləyin!`")  
+    await event.edit(LANG['SEARCHING_SPOT'])  
     dosya = os.getcwd() 
     os.system(f"spotdl --song {cmd} -f {dosya}")
-    await event.edit("`Musiqi yükləndi.`")    
+    await event.edit(LANG['DOWNLOADED'])    
 
     l = glob.glob("*.mp3")
     if len(l) >= 1:
-        await event.edit("Musiqi yüklənir!")
+        await event.edit(LANG['UPLOADING'])
         await event.client.send_file(
             event.chat_id,
             l[0],
@@ -73,7 +98,7 @@ async def port_song(event):
         )
         await event.delete()
     else:
-        await event.edit("`Axtardığınız musiqi tapılmadı ! Təəsüf 😓`")   
+        await event.edit(LANG['NOT_FOUND'])   
         return 
     os.system("rm -rf *.mp3")
     subprocess.check_output("rm -rf *.mp3",shell=True)
@@ -87,22 +112,22 @@ async def songpl(event):
     cmd = event.pattern_match.group(1)
 
     if len(cmd) < 1:
-        await event.edit("İşlədilişi: .songpl spotify playlist url")    
+        await event.edit(LANG['USAGE_PL'])    
 
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("`Playlist axtarılır və yüklənilir zəhmət olmasa gözləyin!`")
+    await event.edit(LANG['SEARCHING_PL'])
     dosya = os.getcwd() + "/playlist/" + "pl.pl"
     klasor = os.getcwd() + "/playlist/"
     sonuc = os.system(f"spotdl --playlist {cmd} --write-to=\"{dosya}\"")
     sonuc2 = os.system(f"spotdl --list {dosya} -f {klasor}")
-    await event.edit("`Axtarılma tamamlandı! İndi yüklənir.`")
+    await event.edit(LANG['DOWNLOADED'])
     l = glob.glob(f"{klasor}/*.mp3")
     i = 0
     if len(l) >= 1:
         while i < len(l):
-            await event.reply("Musiqi göndərilir! Musiqi: " + l[i])
+            await event.reply(LANG['SENDING_SONGS'] + l[i])
             await event.client.send_file(
                 event.chat_id,
                 l[i],
@@ -112,20 +137,17 @@ async def songpl(event):
                 reply_to=reply_to_id
             )
     else:
-        await event.edit("`Axtardığınız playlist tapılmadı ! Təəsüf 😓`")   
+        await event.edit(LANG['NOT_FOUND_PL'])   
         return 
     os.system(f"rm -rf {klasor}/*.mp3")
     subprocess.check_output(f"rm -rf {klasor}/*.mp3",shell=True)
     os.system(f"rm -rf {klasor}/*.pl")
     subprocess.check_output(f"rm -rf {klasor}/*.pl",shell=True)
 
-CMD_HELP.update({'song': 
-    "\n\n`.deez musiqi adı/youtube/spotify/soundcloud`"
-    "\nİşlədilişi: Birçox saytdan musiqi axtarıb, musiqini yükləyər"
-    "\n\n`.song Youtube/Spotify/Musiqi`"
-    "\nİşlədilişi: Musiqi yükləyər"
-    "\n\n`.songpl Spotify Playlist`"
-    "\nİşlədilişi: Spotify Playlist'indən musiqi yükləyər"
-
-
-})
+CmdHelp('song').add_command(
+    'deez', '<musiqi adı/youtube/spotify/soundcloud>', 'Bir çox saytdan musiqini axtarıb, musiqini endirir.'
+).add_command(
+    'song', '<musiqi adı/youtube/spotify>', 'Musiqi endirir.'
+).add_command(
+    'songpl', '<spotify playlist>', 'Spotify Playlist\'indən musiqi endirir'
+).add()
