092c26e41e37ed1243dabeefd318bf5b82d123d1
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-10-31 02:15:13 +0400

    Update autopp.py

--- userbot/modules/autopp.py
+++ userbot/modules/autopp.py
@@ -1,26 +1,18 @@
-# Copyright (C) 2020 TeamDerUntergang.
+# Copyright (C) 2020 Yusuf Usta.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the  GPL-3.0 License;
+# you may not use this file except in compliance with the License.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
+
+# Asena UserBot - Yusuf Usta
+
 # @NaytSeyd tarafından portlanmıştır.
 # @frknkrc44 tarafından düzenlenmiştir.
-#
+# @Fusuf tarafından AutoVideo yazılmıştır.
 
 import os
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
-from pySmartDL import SmartDL
 from telethon.tl import functions
 from telethon.tl.types import InputMessagesFilterDocument
 from userbot import CMD_HELP, AUTO_PP, ASYNC_POOL
@@ -28,25 +20,88 @@ from userbot.events import register
 import asyncio
 import random
 import shutil
+import requests
+import time
+from telethon.errors import VideoFileInvalidError
+from userbot.cmdhelp import CmdHelp
 
-@register(outgoing=True, pattern="^.autopp ?(.*)")
-async def autopic(event):
-    if 'autopic' in ASYNC_POOL:
-        await event.edit("`Profil şəkliniz onsuz avtomatik dəyişir.`")
+# ██████ LANGUAGE CONSTANTS ██████ #
+
+from userbot.language import get_value
+LANG = get_value("autopp")
+
+# ████████████████████████████████ #
+
+# Before kang; please ask to @fusuf :) #
+@register(outgoing=True, pattern="^.autovideo ?(.*)$")
+async def autovideo(event):
+    if 'autovideo' in ASYNC_POOL:
+        await event.edit(LANG['VIDEO_ALREADY_CHANGING'])
         return
 
-    await event.edit("`Profil şəkliniz hazırlanır ...`")
+    if not event.reply_to_msg_id:
+        return await event.edit(LANG['NEED_VIDEO'])
+    else:
+        await event.edit(LANG['SETTING_VIDEO'])
+        
+        # Telethon doesn't support download profile-video so ... #
+        reply = await event.get_reply_message()
+        video = await reply.download_media()
+        yazi = event.pattern_match.group(1)
+
+    try:
+        os.remove("./pp.mp4")
+    except:
+        pass
+
+    try:
+        await event.client(functions.photos.UploadProfilePhotoRequest(
+            video=await event.client.upload_file(video)
+        ))
+    except VideoFileInvalidError:
+        return await event.edit(LANG['INVALID_VIDEO'])
+    
+    ASYNC_POOL.append('autovideo')
+
+    await event.edit(LANG['STARTED_VIDEO'])
+    while "autovideo" in ASYNC_POOL:
+        saat = time.strftime("%H\.%M")
+        tarih = time.strftime("%d\/%m\/%Y")
+
+        if yazi:
+            yazi = yazi.replace("$saat", saat).replace("$tarih", tarih)
+            KOMUT = f"text=\'{yazi}\' :expansion=normal: y=h-line_h-10:x=(mod(5*n\,w+tw)-tw): fontcolor=white: fontsize=30: box=1: boxcolor=black@0.5: boxborderw=5: shadowx=2: shadowy=2"
+        else:
+            KOMUT = f"text=\'Saat\: {saat} Tarih\: {tarih} {yazi}\' :expansion=normal: y=h-line_h-10:x=(mod(5*n\,w+tw)-tw): fontcolor=white: fontsize=30: box=1: boxcolor=black@0.5: boxborderw=5: shadowx=2: shadowy=2"
+
+        ses = await asyncio.create_subprocess_shell(f"ffmpeg -y -i '{video}' -vf drawtext=\"{KOMUT}\" pp.mp4")
+        await ses.communicate()
+
+        await event.client(functions.photos.UploadProfilePhotoRequest(
+            video=await event.client.upload_file("pp.mp4")
+        ))
+        os.remove("./pp.mp4")
+        await asyncio.sleep(60)
+
+    os.remove(video)
+
+@register(outgoing=True, pattern="^.autopp (.*)")
+async def autopic(event):
+    if 'autopic' in ASYNC_POOL:
+        await event.edit(LANG['PHOTO_ALREADY_CHANGING'])
+        return
+
+    await event.edit(LANG['SETTING'])
 
     FONT_FILE_TO_USE = await get_font_file(event.client, "@FontDunyasi")
 
     downloaded_file_name = "./userbot/eskipp.png"
-    downloader = SmartDL(AUTO_PP, downloaded_file_name, progress_bar=True)
-    downloader.start(blocking=False)
-    photo = "yenipp.png"
-    while not downloader.isFinished():
-        continue
+    r = requests.get(AUTO_PP)
 
-    await event.edit("`Profil şəkliniz hazırlandı :)`")
+    with open(downloaded_file_name, 'wb') as f:
+        f.write(r.content)    
+    photo = "yenipp.png"
+    await event.edit(LANG['SETTED'])
 
     ASYNC_POOL.append('autopic')
 
@@ -85,10 +140,12 @@ async def get_font_file(client, channel_id):
     # Dosya yolunu indir ve geri dön
     return await client.download_media(font_file_message)
 
-CMD_HELP.update({
-    "autopp": 
-    ".autopp \
-    \nİşlədilişi: Bu əmr seçdiyiniz şəkli profil şəkli edər \
-    \nvə şəklin altına saat əlavə edər. Bu saat hər dəqiqə dəyişir. \
-    \nMƏLUMAT: Balaca bir ehtimal belə olsa profilinizin blok olması riski var. Bunu diqqətli işlədin."
-})
+CmdHelp('autopp').add_command(
+    'autopp', None, 'Bu komut belirlediğiniz fotoğrafı profil resmi yapar ve bir saat ekler. Bu saat her dakika değişir.', '.autopp'
+).add()
+
+CmdHelp('autovideo').add_command(
+    'autopp', None, 
+    'Bu komut yanıt verdiğiniz videoyu profil video yapar ve bir saat veya tarih veya istediğiniz bir yazı ekler. Bu saat her dakika değişir. nEğer botun kendi yazısını kullanmak istiyorsanız ekstradan bir şey yazmayın. Kendi yazınızı eklemek istiyorsanız .autovideo yazı şeklinde kullanın. ',
+    '.autovideo ahan saat $saat bu da tarih $tarih'
+).add()
