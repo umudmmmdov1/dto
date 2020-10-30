e566ef0952472bf12c8220f8bf372a2910324189
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-10-31 02:53:51 +0400

    Update status.py

--- userbot/modules/status.py
+++ userbot/modules/status.py
@@ -6,7 +6,7 @@
 
 # Asena UserBot - Yusuf Usta
 
-from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, ASENA_VERSION
+from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, DTO_VERSION
 from userbot.events import register
 from telethon import version
 from platform import python_version
