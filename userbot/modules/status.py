a39ad29bfca7233f08cd8767e33dbbb402282ccf
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-10-31 23:36:46 +0400

    Update status.py

--- userbot/modules/status.py
+++ userbot/modules/status.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# DTÖUserBot - Ümüd
 
 from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, DTO_VERSION
 from userbot.events import register
@@ -34,7 +34,7 @@ async def durums(event):
     await event.edit(f"""
 **Python {LANG['VERSION']}:** `{python_version()}`
 **TeleThon {LANG['VERSION']}:** `{version.__version__}` 
-**Asena {LANG['VERSION']}:** `{DTO_VERSION}`
+**DTÖUserBot {LANG['VERSION']}:** `{DTO_VERSION}`
 
 **{LANG['PLUGIN_COUNT']}:** `{len(CMD_HELP)}`
 
@@ -56,5 +56,5 @@ async def durums(event):
     """)
 
 CmdHelp('durum').add_command(
-    'durum', None, 'Eklenen Apiler ve sürümleri gösterir.'
+    'durum', None, 'Əlavə edilən Apilər və versiyaları göstərir.'
 ).add()
