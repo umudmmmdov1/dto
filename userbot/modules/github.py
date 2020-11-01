38e4a57bf5131ed2d8dc3ade4b7b1f7b83e61ce8
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-11-01 23:26:54 +0400

    Update github.py

--- userbot/modules/github.py
+++ userbot/modules/github.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# DTÖUserBot - Ümüd
 
 
 import aiohttp
@@ -64,5 +64,5 @@ async def github(event):
                 await event.edit(REPLY)
 
 CmdHelp('git').add_command(
-    'git', '<kullanıcı adı>', 'Hedeflenen kişinin GitHub bilgilerini gösterir.', 'git quiec'
+    'git', '<istifadəçi adı>', 'Hədəflənən istifadəçinin GitHub məlumatlarını göstərər.', 'git umudmmmdov1'
 ).add()
