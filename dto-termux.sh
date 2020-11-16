834e7c603e5bc711f1eff76d286286fe9dc76d4d
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-11-16 23:28:40 +0400

    Update dto-termux.sh

--- dto-termux.sh
+++ dto-termux.sh
@@ -1,23 +1,23 @@
-MESAJ="DTÖUserBot Yükleyici"
+MESAJ="DTÖUserBot Yükləyici"
 MESAJ+="\nTelegram: @DTOUserBot"
 pkg upgrade
 clear
 echo -e $MESAJ
-echo "Python yüklenir"
+echo "Python yüklənir"
 pkg install python -y
 clear
 echo -e $MESAJ
-echo "TeleThon yüklenir"
+echo "TeleThon yüklənir"
 pip install telethon
-echo "Requests/BS4 yüklenir"
+echo "Requests/BS4 yüklənir"
 pip install requests
 pip install bs4
 clear
 echo -e $MESAJ
 echo "Fayl yazılır..."
-curl "https://raw.githubusercontent.com/umudmmmdov1/DunyaTurkOrgutu/master/dto.py" --output "dto.py"
+curl "https://raw.githubusercontent.com/umudmmmdov1/DTOUserBot/master/dto.py" --output "dto.py"
 clear
 echo -e $MESAJ
-echo "Qurulum hazırdır, İndi StringSessionu ala bilersiz"
+echo "Qurulum hazırdır, İndi StringSessionu ala bilərsiz"
 clear
 python dto.py
