400c7e789e9abee7061e7d0664f9401cdfc8e627
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-11-16 23:24:44 +0400

    Add files via upload

---
+++ dto-ios.sh
@@ -0,0 +1,20 @@
+mesaj = "DTÖUserBot Yükleyici"
+mesaj += "Telegram: @DTOUserBot"
+mesaj += "Çıxan her şeye Y ardından enterleyin."
+clear
+echo $mesaj
+echo "Python yüklenir"
+apk add python3
+clear
+echo $mesaj
+echo "TeleThon yüklenir"
+pip3 install telethon
+pip3 install bs4
+pip3 install requests
+clear
+echo $mesaj
+echo "Fayl yazılır"
+curl "https://raw.githubusercontent.com/umudmmmdov1/DunyaTurkOrgutu/master/dto.py" --output "dto.py"
+echo $mesaj
+echo "Qurulum hazırdır, İndi StringSessionu ala bilersiz"
+python3 dto.py
---
+++ dto-termux.sh
@@ -0,0 +1,23 @@
+MESAJ="DTÖUserBot Yükleyici"
+MESAJ+="\nTelegram: @DTOUserBot"
+pkg upgrade
+clear
+echo -e $MESAJ
+echo "Python yüklenir"
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "TeleThon yüklenir"
+pip install telethon
+echo "Requests/BS4 yüklenir"
+pip install requests
+pip install bs4
+clear
+echo -e $MESAJ
+echo "Fayl yazılır..."
+curl "https://raw.githubusercontent.com/umudmmmdov1/DunyaTurkOrgutu/master/dto.py" --output "dto.py"
+clear
+echo -e $MESAJ
+echo "Qurulum hazırdır, İndi StringSessionu ala bilersiz"
+clear
+python dto.py
