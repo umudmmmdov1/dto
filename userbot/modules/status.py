8d46a8200136f3fa37352b3ff2fff3cda890252c
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-12-12 20:13:17 +0400

    Update status.py

--- userbot/modules/status.py
+++ userbot/modules/status.py
@@ -1,9 +1,4 @@
 # Copyright (C) 2020
-#
-# Licensed under the GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-
 # DTÖUserBot - Ümüd
 
 from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, DTO_VERSION
