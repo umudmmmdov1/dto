eb33b7d6eda2e996f57856173d58797afb54e025
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-12-17 16:13:32 +0400

    Delete windows_startup_script.py

--- windows_startup_script.py
+++
@@ -1,6 +0,0 @@
-from telethon import TelegramClient, events
-API_KEY="Bunu bura yazın"
-API_HASH="Bunu bura yazın"
-#my.telegram.org adresinden alın
-bot = TelegramClient('userbot',API_KEY,API_HASH)
-bot.start()
