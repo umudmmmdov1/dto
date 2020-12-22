66b4f3baef0d1560a0c6cfb4a26221e6544dc46e
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-12-23 03:17:19 +0400

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -74,8 +74,8 @@ async def amireallyalive(alive):
 async def amireallyaliveuser(username):
     """ Alive """
     message = username.text
-    output = ".alivead [yeni ad] boş ola bilməz"
-    if message != ".alivead" mesaj [7:8] == " ":
+    output = ".alivead [mötərəzəsiz] və boş ad ola bilməz"
+    if message != ".alivead mesaj [7:8] == " ":
         newuser = message[8:]
         global DEFAULTUSER
         DEFAULTUSER = newuser
@@ -88,4 +88,4 @@ async def amireallyalivereset(ureset):
     """ Alive """
     global DEFAULTUSER
     DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-    await ureset.edit("`" "Uğurla silindi!" "`")
+    await ureset.edit("`" "Uğurla alive ad silindi!" "`")
