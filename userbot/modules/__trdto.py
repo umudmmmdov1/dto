ed46b36085dd441174397c73a7dbbd14b04bb281
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-12-28 20:20:00 +0400

    Create __trdto.py

---
+++ userbot/modules/__trdto.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2020
+# DTÖUserBot - Ümüd
+
+from userbot.cmdtr import CmdTr
+from userbot import cmdhelp
+from userbot import CMD_HELPTR
+from userbot.events import register
+
+# ██████ LANGUAGE CONSTANTS ██████ #
+
+from userbot.language import get_value
+LANG = get_value("__trdto")
+
+# ████████████████████████████████ #
+
+@register(outgoing=True, pattern="^.trdto(?: |$)(.*)")
+async def trdto(event):
+
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELPTR:
+            await event.edit(str(CMD_HELPTR[args]))
+        else:
+            await event.edit(LANG["NEED_PLUGIN"])
+    else:
+        string = ""
+        sayfa = [list(CMD_HELPTR)[i:i + 5] for i in range(0, len(list(CMD_HELPTR)), 5)]
+        
+        for i in sayfa:
+            string += f'`❄️ `'
+            for sira, a in enumerate(i):
+                string += "__" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "__"
+                else:
+                    string += "`, "
+            string += "\n"
+        await event.edit(LANG["NEED_MODULE"] + '\n\n' + string)
