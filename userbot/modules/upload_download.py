519053f76cc6e520e7ebba7ac30446942b8b12e2
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-01-01 02:50:36 +0400

    Update upload_download.py

--- userbot/modules/upload_download.py
+++ userbot/modules/upload_download.py
@@ -1,13 +1,7 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# DTÖUserBot - Ümüd
+# U S Σ R Δ T O R / Ümüd
 
 
-"""  """
+""" Upload """
 
 import json
 import os
@@ -26,6 +20,7 @@ from telethon.tl.types import DocumentAttributeVideo
 from userbot import LOGS, CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, BOT_USERNAME
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
+from userbot.cmdtr import CmdTr
 
 # ██████ LANGUAGE CONSTANTS ██████ #
 
@@ -589,4 +584,16 @@ CmdHelp('updown').add_command(
     'unzip', '<cavab>', 'Cavab verdiyiniz Zip faylını çıxardar.'
 ).add_command(
     'uploadir', '<qovluq>', 'Bütün qovluğu yükləyər.'
+).add()
+
+CmdTr('updown').add_command(
+    'download', '<bağlantı-dosya adı> (ya da bir şeye cevap vererek)', 'Sunucuya dosyayı indirir.'
+).add_command(
+    'upload', '<sunucudaki dosya yolu>', 'Sunucunuzdaki bir dosyayı sohbete upload eder.'
+).add_command(
+    'wupload', ' <dosyaya yanıt verin> anonfiles|transfer|filebin|tmpninja|anonymousfiles|megaupload|bayfiles|letsupload|vshare', 'Seçtiğiniz websiteye yükler.'
+).add_command(
+    'unzip', '<yanıt>', 'Yanıt verdiğiniz Zip dosyasını çıkarır.'
+).add_command(
+    'uploadir', '<klasör>', 'Tüm klasöru yükler.'
 ).add()
