d3c49604acf3d07e8c26eeb8a94bdf18ea79fd89
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-01-03 22:42:41 +0400

    Update __dto.py

--- userbot/modules/__dto.py
+++ userbot/modules/__dto.py
@@ -14,24 +14,26 @@ LANG = get_value("__dto")
 
 @register(outgoing=True, pattern="^.dto(?: |$)(.*)")
 async def dto(event):
-
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit(LANG["NEED_PLUGIN"])
+            await event.edit(["NEED_PLUGIN"])
     else:
         string = ""
-        sayfa = [list(CMD_HELP)[i:i + 5] for i in range(0, len(list(CMD_HELP)), 5)]
-        
+        sayfa = [
+            sorted(list(CMD_HELP))[i : i + 5]
+            for i in range(0, len(sorted(list(CMD_HELP))), 5)
+        ]
+
         for i in sayfa:
-            string += f'`➤ `'
+            string += f"`➤ `"
             for sira, a in enumerate(i):
-                string += "__" + str(a)
+                string += "`" + str(a)
                 if sira == i.index(i[-1]):
-                    string += "__"
+                    string += "`"
                 else:
                     string += "`, "
             string += "\n"
-        await event.edit(LANG["NEED_MODULE"] + '\n\n' + string)
+        await event.edit(["NEED_MODULE"] + "\n\n" + string)
