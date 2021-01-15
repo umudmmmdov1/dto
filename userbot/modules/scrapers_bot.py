8fb714a829c092243dc9783d4330bbb7c9b0aaef
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-01-15 21:33:03 +0400

    Update scrapers_bot.py

--- userbot/modules/scrapers_bot.py
+++ userbot/modules/scrapers_bot.py
@@ -14,7 +14,6 @@ from userbot.modules.admin import get_user_from_event
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 from userbot.cmdhelp import CmdHelp
-from userbot.cmdtr import CmdTr
 
 # ██████ LANGUAGE CONSTANTS ██████ #
 
@@ -391,20 +390,4 @@ CmdHelp('scrapers_bot').add_command(
     'ocr2', '<cavab>', 'Fotodakı yazını oxuyun.'
 ).add_command(
     'creation', '<cavab>', 'Cavab verdiyiniz insanın hesabının yaradılış tarixini öyrənin.'
-).add()
-
-CmdTr('scrapers_bot').add_command(
-    'sangmata', '<yanıt>', 'Belirtilen kullanıcının isim geçmişini görüntüleyin.'
-).add_command(
-    'scan', '<yanıt>', 'Belirtilen dosyada virüs var mı yok mu bakın.'
-).add_command(
-    'meme', '<font> <üst;alt>', 'Fotoğrafa yazı ekleyin. İsterseniz font büyüklüğünü de yazabilirsiniz.', 'meme 30 asena;usta'
-).add_command(
-    'voicy', '<yanıt>', 'Sesi yazıya çevirin.'
-).add_command(
-    'q', '<sayı>', 'Metninizi çıkartmaya dönüştürün.'
-).add_command(
-    'ocr2', '<yanıt>', 'Fotoğraftaki metini okuyun.'
-).add_command(
-    'creation', '<yanıt>', 'Yanıt verdiğiniz kişinin hesabı oluşturma tarihinin öğrenin.'
 ).add()
