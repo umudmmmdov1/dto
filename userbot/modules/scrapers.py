872d9f302692545a6393e5e10b66b4f2987acab8
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-01-25 02:19:31 +0400

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -476,33 +476,47 @@ async def imdb(e):
         await e.edit("Keçərli bir film adı yaz.")
 
 @register(outgoing=True, pattern=r"^\.trt(?: |$)([\s\S]*)")
-async def translateme(trans):
-    """ .trt """
-    translator = Translator()
-    textx = await trans.get_reply_message()
-    message = trans.pattern_match.group(1)
-    if message:
-        pass
-    elif textx:
-        message = textx.text
+async def _(event):
+    if event.fwd_from:
+        return
+    if "trim" in event.raw_text:
+        return
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str or "en"
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
     else:
-        return await trans.edit("`Mənə tərcümə olunacaq mesaj ver!`")
+        await edit_or_reply(event, "`Mənə tərcümə olunacaq mesaj ver!`")
+        return
 
+    lan = lan.strip()
     try:
-        reply_text = translator.translate(deEmojify(message), dest=TRT_LANG)
-    except ValueError:
-        return await trans.edit("Yalnış dil kodu yazmısız təkrar edin.")
+        translator = Translator()
+        translated = translator.translate(text, lang_tgt=lan)
+        lmao = detect(text)
+        after_tr_text = lmao
+        source_lan = LANGUAGES[after_tr_text]
+        transl_lan = LANGUAGES[lan]
+        output_str = f"""**Tərcümə olunur...**
+**Bu dildən ({source_lan})**:
+`{text}`
+**Bu dilə ({transl_lan})**:
+`{translated}`"""
 
-    source_lan = LANGUAGES[f"{reply_text.src.lower()}"]
-    transl_lan = LANGUAGES[f"{reply_text.dest.lower()}"]
-    reply_text = f"Bu dildən ➡️ **{source_lan.title()}**\nBu dilə ➡️ **{transl_lan.title()}:**\n\n{reply_text.text}"
-
-    await trans.edit(reply_text)
-    if BOTLOG:
-        await trans.client.send_message(
-            BOTLOG_CHATID,
-            f"Tərcümə olundu {source_lan.title()} bu {transl_lan.title()} sözə.",
-        )
+        if len(output_str) >= 4096:
+            out_file = output_str
+            url = "https://del.dog/documents"
+            r = requests.post(url, data=out_file.encode("UTF-8")).json()
+            url2 = f"https://del.dog/{r['key']}"
+            starky = f"Tərcümə olunacaq mesaj çox böyükd7r, Ama bunu linklə yollaya bilərsən [tıkla]({url2})"
+        else:
+            starky = output_str
+        await edit_or_reply(event, starky)
+    except Exception as e:
+        print(e)
 
     
 @register(pattern=".lang (trt|tts) (.*)", outgoing=True)
