015e45a9376f659add0d516657d54afa0ac50804
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-01-29 23:07:59 +0400

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -28,8 +28,7 @@ from requests import get
 from search_engine_parser import GoogleSearch
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google_trans_new import google_translator
-from googletrans import LANGUAGES
+from google_trans_new import LANGUAGES, google_translator
 from langdetect import detect
 from gtts import gTTS
 from gtts.lang import tts_langs
