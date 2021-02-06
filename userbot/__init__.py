8781f543c65cedee30f9a8ed6e887d0c1f89c834
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-02-07 02:04:51 +0400

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -62,7 +62,7 @@ if not LANGUAGE in ["EN", "TR", "AZ", "UZ", "DEFAULT"]:
     LANGUAGE = "DEFAULT"
     
 # DTÖ Versiyası
-DTO_VERSION = "2.6"
+DTO_VERSION = "2.7"
 
 # Telegram API KEY ve HASH
 API_KEY = os.environ.get("API_KEY", None)
