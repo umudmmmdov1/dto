8c16d5e63201347e3d710bad9a7a1dee9e9054b8
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-04-14 01:44:30 +0400

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -319,4 +319,4 @@ async def type_afk_is_not_true(notafk):
         USERS = {}
         AFKREASON = None
 
-CmdHelp('afk').add_command('afk', None, {LANG[AFK]}).add()
+CmdHelp('afk').add_command('afk', None, (LANG['AFK'])).add()
