a76ea2e3d17fd4b0b87dc33e1c3dd2ec461fcba0
coshgyn <74981823+coshgyn@users.noreply.github.com>
2021-06-07 00:23:31 +0400

    yeni song modulu

--- userbot/modules/song.py
+++ userbot/modules/song.py
@@ -2,6 +2,20 @@
 
 import datetime
 import asyncio
+import json, random
+from telethon.tl.types import DocumentAttributeAudio
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (
+    ContentTooShortError,
+    DownloadError,
+    ExtractorError,
+    GeoRestrictedError,
+    MaxDownloadsReached,
+    PostProcessingError,
+    UnavailableVideoError,
+    XAttrMetadataError,
+)
+from youtubesearchpython import SearchVideos
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
@@ -62,40 +76,114 @@ async def deezl(event):
             await event.client.send_message(event.chat_id, f"`{sarkilar.buttons[sira][0].text}` | " + LANG['UPLOADED_WITH'], file=sarki.message)
             await event.delete()
 
-@register(outgoing=True, pattern="^.song(?: |$)(.*)")
-async def port_song(event):
-    if event.fwd_from:
+@register(outgoing=True, pattern="^.song ?(.*)")
+@register(incoming=True, from_users=SUDO_ID, pattern="^.song ?(.*)")
+async def song(event):
+    a = event.text
+    if a[5] == "s":
         return
-    
-    cmd = event.pattern_match.group(1)
-    if len(cmd) < 1:
-        await event.edit(LANG['UPLOADED_WITH']) 
-
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-        
-    await event.edit(LANG['SEARCHING_SPOT'])  
-    dosya = os.getcwd() 
-    os.system(f"spotdl --song {cmd} -f {dosya}")
-    await event.edit(LANG['DOWNLOADED'])    
-
-    l = glob.glob("*.mp3")
-    if len(l) >= 1:
-        await event.edit(LANG['UPLOADING'])
-        await event.client.send_file(
-            event.chat_id,
-            l[0],
-            force_document=True,
-            allow_cache=False,
-            reply_to=reply_to_id
+    x = await event.edit(LANG['SEARCHING'])
+    url = event.pattern_match.group(1)
+    if not url:
+        return await x.edit(LANG['USAGE'])
+    search = SearchVideos(url, offset=1, mode="json", max_results=1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get("search_result")
+    try:
+        url = q[0]["link"]
+    except BaseException:
+        return await x.edit(LANG['NOT_FOUND'])
+    type = "audio"
+    await x.edit(f"`{url} Yüklənməyə hazırlanır...` Ümüd bunlara lang əlavə elə")
+    if type == "audio":
+        opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                }
+            ],
+            "outtmpl": "%(id)s.mp3",
+            "quiet": True,
+            "logtostderr": False,
+        }
+    try:
+        await x.edit("`Məlumatlar gətirilir...`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await x.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await x.edit("`Yüklənəcək media çox qısadır`")
+        return
+    except GeoRestrictedError:
+        await x.edit(
+            "`coğrafi...`"
         )
-        await event.delete()
+        return
+    except MaxDownloadsReached:
+        await x.edit("`Max yükləmə limitini aşdınız`")
+        return
+    except PostProcessingError:
+        await x.edit("`Bir xəta baş verdi.`")
+        return
+    except UnavailableVideoError:
+        await x.edit("`Media bu formatı dəstəkləmir.`")
+        return
+    except XAttrMetadataError as XAME:
+        await x.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await x.edit("`Yükləmə zamanı bir xəta baş verdi`")
+        return
+    except Exception as e:
+        return await x.edit(f"{str(type(e)): {str(e)}}")
+    dir = os.listdir()
+    if f"{rip_data['id']}.mp3.jpg" in dir:
+        thumb = f"{rip_data['id']}.mp3.jpg"
+    elif f"{rip_data['id']}.mp3.webp" in dir:
+        thumb = f"{rip_data['id']}.mp3.webp"
     else:
-        await event.edit(LANG['NOT_FOUND'])   
-        return 
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
+        thumb = None
+    upteload = """
+Yüklənilir...
+Musiqi adı - {}
+{} tərəfindən
+""".format(
+        rip_data["title"], rip_data["uploader"]
+    )
+    await x.edit(f"`{upteload}`")
+    CAPT = f"» **{rip_data['title']}**" + LANG['UPLOADED_WITH']
+    await bot.send_file(
+        event.chat_id,
+        f"{rip_data['id']}.mp3",
+        thumb=thumb,
+        supports_streaming=True,
+        caption=CAPT,
+        attributes=[
+            DocumentAttributeAudio(
+                duration=int(rip_data["duration"]),
+                title=str(rip_data["title"]),
+                performer=str(rip_data["uploader"]),
+            )
+        ],
+    )
+    await x.delete()
+    os.remove(f"{rip_data['id']}.mp3")
+    try:
+        os.remove(thumb)
+    except BaseException:
+        pass
+# ported from Ultroid
 
 @register(outgoing=True, pattern="^.songpl ?(.*)")
 async def songpl(event):
