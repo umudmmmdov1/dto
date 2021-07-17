2e00629fe5517b43f7ae0b006c1ab75946b53d63
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2021-07-18 01:54:49 +0400

    .dto dizayn dəyişiklik

--- userbot/modules/__dto.py
+++ userbot/modules/__dto.py
@@ -25,7 +25,7 @@ async def dto(event):
         sayfa = [sorted(list(CMD_HELP))[i:i + 5] for i in range(0, len(sorted(list(CMD_HELP))), 5)]
         
         for i in sayfa:
-            string += f'`➤ `'
+            string += f'`💝`'
             for sira, a in enumerate(i):
                 string += "`" + str(a)
                 if sira == i.index(i[-1]):
